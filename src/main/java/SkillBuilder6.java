d866f547e5dc0c62cd8113f903d138feaa28169f
DsuAlex <156232714+DsuAlex@users.noreply.github.com>
2024-11-25 23:50:15 -0500

    done

--- src/main/java/SkillBuilder6.java
+++ src/main/java/SkillBuilder6.java
@@ -1,34 +1,52 @@
-
-/**
- * SkillBuilder6 is a class for completing the Skill
- * Builder 6 assignment in Java.
- *
- * @author <You>
- * @version 1.0
- */
-
 public class SkillBuilder6 {
 
     private static final int start = 0;
     private static final int T = 1;
     private static final int Y = 2;
 
-
-    public static String findTYPattern(String s)
-    {   int i;
+    public static String findTYPattern(String s) {
         String sub = "";
         int CS = start;
-        for (i = 0; i < s.length() || CS != Y; i++);
-        char L = s.charAt(i);
-        if (L == 't' || L =='T'){
-            CS = T;
-            if (CS == T){
-                sub += L;
+
+
+        for (int i = 0; i < s.length(); i++) {
+            char L = s.charAt(i);
+
+            if (CS == start) {
+                if (L == 't' || L == 'T') {
+                    CS = T;
+                    sub += L;
+                }
             }
-            if (L =='y' || L == 'Y' && CS == T)
-                CS = Y;
+
+            else if (CS == T) {
+                sub += L;
+
+                if (L == 'y' || L == 'Y') {
+                    CS = Y;
+                }
+            }
+
+
+            if (CS == T && i == s.length() - 1) {
+                sub = "";
+            }
+
+
+            if (CS == Y) {
+                break;
+            }
         }
-        return sub;
+
+
+        return sub.length() > 0 ? sub : "";
     }
 
 }
+
+
+
+
+
+
+
